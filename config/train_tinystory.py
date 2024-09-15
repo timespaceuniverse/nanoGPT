61ddea1ed3c71d5d17f8283b61885441476229b2
temp <temp@xs-MacBook-Pro.local>
2024-09-14 19:20:37 -0700

    update

--- config/train_tinystory.py
+++ config/train_tinystory.py
@@ -1,37 +1,29 @@
-# train a miniature character-level shakespeare model
-# good for debugging and playing on macbooks and such
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 out_dir = 'out-tinystory'
-eval_interval = 250 # keep frequent because we'll overfit
-eval_iters = 200
-log_interval = 10 # don't print too too often
-
-# we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
-
-wandb_log = False # override via command line if you like
-wandb_project = 'tinystory'
-wandb_run_name = 'mini-gpt'
 
 dataset = 'tinystory'
-gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
 
-# baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+wandb_log = False
+wandb_project = 'tinystory'
+wandb_run_name='tinystory'
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
-beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+# these make the total batch size be ~0.5M
+# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
+batch_size = 12
+block_size = 1024
+gradient_accumulation_steps = 5 * 8
 
-warmup_iters = 100 # not super necessary potentially
+# this makes total number of tokens be 300B
+max_iters = 600000
+lr_decay_iters = 600000
 
-# on macbook also add
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+# eval stuff
+eval_interval = 1000
+eval_iters = 200
+log_interval = 10
+
+# weight decay
+weight_decay = 1e-1
